8944eaccd0ee9f8da6447b6cae61f0d7b74f565e
sportsman701 <sportsman701@outlook.com>
2019-02-05 12:15:49 -0500

    trgin to get geocode to compile

--- resources/js/components/Geocode.js
+++ resources/js/components/Geocode.js
@@ -1,32 +1,32 @@
-// import React, { Component } from 'react'
-// import Geocode from "react-geocode";
-// import Map from "./Map.js"
-// import Bookingpages from "./Bookingpages.js"
+import React, { Component } from 'react'
+import Geocode from "react-geocode";
+import Map from "./Map.js"
+import Bookingpages from "./Bookingpages.js"
 
 
-// export default class Geocode extends Component {
+export default class Geocode extends Component {
 
   
-//   render() {
+  render() {
 
-//   Geocode.fromAddress(this.props.origin).then(
-//   response => {
-//     const { lat, lng } = response.results[0].geometry.location;
-//     console.log (lat, lng);
+  Geocode.fromAddress(this.props.origin).then(
+  response => {
+    const { lat, lng } = response.results[0].geometry.location;
+    console.log (lat, lng);
   
-//   },
-//   error =>{
-//     console.log(error);
-//   }
-// );
+  },
+  error =>{
+    console.log(error);
+  }
+);
 
-//     return (
-//       <div>
+    return (
+      <div>
         
-//       </div>
-//     )
-//   }
-// }
+      </div>
+    )
+  }
+}
 
 
 
