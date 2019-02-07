6ad7cb95fa1af9456212a4e49872a88efc89c1ac
sportsman701 <sportsman701@outlook.com>
2019-02-06 20:14:31 -0500

    Add map logic for Booking component

---
+++ resources/js/components/LocationSearchInput.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
+class LocationSearchInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    handleChange = address => {
+        const event = {
+            target: { name: this.props.name, value: address }
+        };
+
+        this.props.handleChange(event);
+    };
+
+    handleSelect = address => {
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => {
+                this.props.handleAutocompleteSelect({ name: this.props.name, coordinates: latLng, address })
+            })
+            .catch(error => console.error('Error', error));
+    };
+
+    render() {
+        return (
+            <PlacesAutocomplete
+                value={this.props.value}
+                onChange={this.handleChange}
+                onSelect={this.handleSelect}
+            >
+                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                    <div>
+                        <input
+                            {...getInputProps({
+                                placeholder: `Enter ${this.props.name}`,
+                                className: 'form-control',
+                            })}
+                        />
+                        <div className="autocomplete-dropdown-container">
+                            {loading && <div>Loading...</div>}
+                            {suggestions.map(suggestion => {
+                                const className = suggestion.active
+                                    ? 'suggestion-item--active'
+                                    : 'suggestion-item';
+                                // inline style for demonstration purpose
+                                const style = suggestion.active
+                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                return (
+                                    <div
+                                        {...getSuggestionItemProps(suggestion, {
+                                            className,
+                                            style,
+                                        })}
+                                    >
+                                        <span>{suggestion.description}</span>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </PlacesAutocomplete>
+        );
+    }
+}
+
+export default LocationSearchInput;
