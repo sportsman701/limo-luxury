44a1ebd5382ddbb93cd1f54767a6ea58d50a8c64
sportsman701 <sportsman701@outlook.com>
2019-02-13 14:52:09 -0500

    working on state

--- resources/js/components/VehicleCardNew.js
+++ resources/js/components/VehicleCardNew.js
@@ -9,7 +9,8 @@ export default class VehicleCard extends Component {
     constructor (props) {
       super(props);
       this.state = {
-         name: ""
+         name: "",
+         value: ""
       }
       this.onClick = this.onClick.bind(this);
       
@@ -20,38 +21,41 @@ export default class VehicleCard extends Component {
           name: this.state.value
         });
 
-  
-    return (
+
+  render() {
+       return (
  
-        <div className="card">
-        <div className="img-container">
-        <img alt={props.name} src={props.image} />
-        </div>
-        <div className="content">
-          <ul>
-            <li>
-              <strong>Name:</strong> {props.name}
-            </li>
-            <li>
-              <strong>Description:</strong> {props.description}
-            </li>
-          </ul>
+            <div className="card">
+            <div className="img-container">
+            <img alt={props.name} src={props.image} />
+            </div>
+            <div className="content">
+              <ul>
+                <li>
+                  <strong>Name:</strong> {props.name}
+                </li>
+                <li>
+                  <strong>Description:</strong> {props.description}
+                </li>
+              </ul>
 
-        <div class="text-center">
-          <Button
-                  variant="primary"
-                  name="VehicleSelect"
-                  type="submit"
-                  value="submit"
-                  onClick={this.onClick} >
-                  {this.setState({ [name]: value })}
-                  SELECT
-          </Button>
-          </div>
-        </div>
+              <div class="text-center">
+                <Button
+                        variant="primary"
+                        name="VehicleSelect"
+                        type="submit"
+                        value="submit"
+                        onClick={this.onClick} >
+                        {this.setState({ name: this.state.value }) }
+                        
+                        SELECT
+                </Button>
+                </div>
+              </div>
 
-      </div>
-    )
+            </div>
+    );
   }
-
+ }
 }
+
